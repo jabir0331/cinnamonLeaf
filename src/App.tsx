0c9d55c9b9af1f8199753d4ad51c90bcdecc7bd9
jabir0331 <mnnjabir@gmail.com>
2025-08-19 01:58:48 +0530

    finally modified the app.tsx file to adjust the layout to diplay the OrderSuccess UI

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,25 @@ import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
 import Testimonials from './pages/Testimonials';
 import Reservation from './pages/Reservation';
+import OrderSuccess from './pages/OrderSuccess';
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-cream-50">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/testimonials" element={<Testimonials />} />
-            <Route path="/reservation" element={<Reservation />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          {/* Routes that use the Layout component */}
+          <Route path="/" element={<Layout><Home /></Layout>} />
+          <Route path="/about" element={<Layout><About /></Layout>} />
+          <Route path="/menu" element={<Layout><Menu /></Layout>} />
+          <Route path="/gallery" element={<Layout><Gallery /></Layout>} />
+          <Route path="/contact" element={<Layout><Contact /></Layout>} />
+          <Route path="/testimonials" element={<Layout><Testimonials /></Layout>} />
+          <Route path="/reservation" element={<Layout><Reservation /></Layout>} />
+          
+          {/* Routes that don't use the Layout component */}
+          <Route path="/order-success" element={<OrderSuccess />} />
+        </Routes>
       </div>
     </Router>
   );
